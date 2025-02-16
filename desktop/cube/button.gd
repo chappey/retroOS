bfc7c1491dc96646be9ae1cf536426d60f7ce9be
kyle arquilla <k2snowboarder21@gmail.com>
2025-02-16 06:39:06 -0500

    working submenus!! and cube color!!

--- desktop/cube/button.gd
+++ desktop/cube/button.gd
@@ -1,11 +1,9 @@
-extends Node3D
+extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$CSGBox3D.rotate_y(0.01)
-	$CSGBox3D.rotate_z(0.01)
+	pass
--- desktop/desktop.gd
+++ desktop/desktop.gd
@@ -3,16 +3,24 @@ extends Control
 
 @onready var taskbar := $Taskbar
 @onready var menu_button := $Taskbar/MenuButton
+@onready var game_menu_button := $Taskbar/MenuButton/MenuButton
+#@onready menu_button.add_submenu_item("Games", game_menu_button)
 
-enum Progs {ABOUT=4, AI=1, TXT_ED=2, CUBE=3, TICTAC=5}
+enum Progs { PRGS=0, AI=1, TXT_ED=2, CUBE=3, ABOUT=4,}
+enum Games {TicTac=0}
 
-@onready var window: Array[Window] = [null, $AIWindow, $TxtEdWindow, $CubeWindow, $AboutWindow]
-
-
+@onready var window: Array[Window] = [null, $AIWindow, $TxtEdWindow, $CubeWindow, $AboutWindow, $TicTacWindow]
 
 
 func _ready() -> void:
+	var submenu = PopupMenu.new()
+	submenu.set_name("submenu")
+	submenu.add_item("Tic Tac Toe",0)
+	submenu.id_pressed.connect(subMenu)
+	menu_button.get_popup().add_child(submenu)
+	menu_button.get_popup().add_submenu_item("Game", "submenu")
 	menu_button.get_popup().connect(&"id_pressed", func(id: int):
+		print(id)
 		match id:
 			Progs.ABOUT:
 				window[Progs.ABOUT].show()
@@ -22,4 +30,12 @@ func _ready() -> void:
 				window[Progs.TXT_ED].show()
 			Progs.CUBE:
 				window[Progs.CUBE].show()
-		)
+			#Progs.PRGS:
+	)
+	
+func subMenu(id):
+	match id:
+		0:	
+			window[5].show()
+			
+		
